d5e50661d12ad5f9b8a0673710afe626d99e66bb
anirudh <anirudhkolgaonkar@gmail.com>
2019-11-30 18:48:29 +0530

    final commit

--- app/src/main/java/android/app/com/leftrightmind/DetailActivity/DetailPage.kt
+++ app/src/main/java/android/app/com/leftrightmind/DetailActivity/DetailPage.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.app.com.leftrightmind.R
+import com.bumptech.glide.Glide
 
 import kotlinx.android.synthetic.main.activity_detail_page.*
+import kotlinx.android.synthetic.main.content_detail_page.*
 
 class DetailPage : AppCompatActivity() {
 
@@ -14,9 +16,25 @@ class DetailPage : AppCompatActivity() {
         setContentView(R.layout.activity_detail_page)
         setSupportActionBar(toolbar)
 
-        supportActionBar!!.setDisplayShowTitleEnabled(false)
         toolbar.setNavigationIcon(R.drawable.ic_back)
         toolbar.setNavigationOnClickListener { finish() }
+
+        val i = intent.extras
+        val uname = i.getString("name","")
+        val imageUrl = i.getString("imageUrl","")
+        val mobile = i.getString("mobile","")
+        val uaddress = i.getString("address","")
+
+        Glide
+            .with(this)
+            .load(imageUrl)
+            .error(R.drawable.ic_placeholder)
+            .into(imageView)
+
+        name.text = uname
+        mobileNo.text = mobile
+        address.text = uaddress
+
     }
 
 }
